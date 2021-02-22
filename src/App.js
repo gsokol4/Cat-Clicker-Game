c493b5cc135ac34798f2477b9a6347fae192fc83
Gabriel Sokol <gsokol4@gmail.com>
2021-02-22 01:46:11 -0600

    button timer is now fully funcional next thing to work on is anding it to the game component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css'
 import Ez from './components/Ez'
 import Game from './components/Game'
 import WindowWidth from './components/WindowWidth'
+import NewClicker from './components/NewClicker'
 
 function App () {
   return (
     <div className='App'>
-      <Ez />
+      <NewClicker />
     </div>
   )
 }
---
+++ src/components/NewClicker.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+
+const timerStyle = {
+  color: 'red'
+}
+const notClickedStyle = {
+  color: 'black'
+}
+export default function Clicker (props) {
+  const [timer, setTimer] = useState(false)
+  const [counter, setCounter] = useState(5)
+  function handleClick () {
+    setTimer(true)
+    console.log(timer, counter)
+  }
+  const handleCounter = () => {
+    setCounter(counter - 1)
+    if (counter < 1) {
+      setTimer(false)
+      setCounter(6)
+    }
+  }
+  const cooldown = () => {
+    let sec
+    let min
+    let hour
+    let addZero
+    let addZeroMin
+    addZero = (counter % 60 < 10 || counter) < 10 ? '0' : ''
+    addZeroMin = (Math.floor((counter / 60) % 60)) < 10 ? '0' : ''
+    sec = `cooldown ${addZero}${counter}s`
+    min = `cooldown ${addZeroMin}${Math.floor(counter / 60)}:${addZero}${counter % 60}`
+    hour = (
+    `cooldown ${Math.floor((counter / 60) / 60)}:
+    ${addZeroMin}${Math.floor((counter / 60) % 60)}:
+    ${addZero}${Math.floor((counter % 60))}`)
+
+    if (counter < 60) {
+      return sec
+    } else if (counter < 3600) {
+      return min
+    } else if (counter > 3600) { return hour }
+
+    return counter
+  }
+  useEffect(() => {
+    const interval = setInterval(() => handleCounter(), 1000)
+    console.log()
+    return () => {
+      clearInterval(interval)
+    }
+  }, [counter])
+
+  let functionCalled = timer ? () => {} : () => { handleClick() }
+  return (
+    <div>
+      <button
+        style={timer ? timerStyle : notClickedStyle}
+        onClick={functionCalled}
+      >
+        {timer ? cooldown() : 'get a box of kitties'}
+      </button>
+    </div>
+  )
+}
