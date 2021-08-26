cc7cbab25973f8ae4a15f0f467941dedc7c2de7f
Gabriel Sokol <gsokol4@gmail.com>
2021-08-26 13:57:26 -0500

    completed changes for entering username

--- src/App.js
+++ src/App.js
@@ -14,17 +14,38 @@ const background = {
   color: 'white'
 }
 function App () {
-  let adjList = ['quick', 'fat', 'sneaky', 'smart', 'abrasive', 'ambiguous', 'deadly', 'determined', 'bawdy', 'cloistered', 'capricious', 'craven', 'dapper', 'debonair','divirgent', 'draconian', 'elated', 'erratic', 'fallacious', 'garrulous', 'great', 'greedy', 'incandescent', 'languid', 'macabre', 'twisted', 'green', 'blue', 'black', 'grey', 'pink', ]
-  let nounList = ['fox', 'cat', 'grandma', 'grandpa', 'dog', 'hound', 'chicken', 'chupacabra', 'lampost', 'rooster', 'wolf', 'vampire', 'bannana', 'peach', 'pear', 'apple', 'duck', 'driver', 'nurse', 'nutcase', 'elephant', 'rat', 'mouse', 'lion', 'kitty', 'kitten', 'cub', 'llama', 'mom', 'dad', 'sister', 'panda', 'sheep', 'tree', 'fern', 'lilly']
+  function isLocalStorageSupported () {
+    var storage = window.localStorage
+    try {
+      storage.setItem('test', 'test')
+      storage.removeItem('test')
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+  function checkLocalStorageNameVar () {
+    if (localStorage.hasOwnProperty('catClickerUserName')) {
+      console.log('yayyy there is a name')
+      return true
+    } else {
+      console.log('wot no name found')
+      return false
+    }
+  }
+  const adjList = ['quick', 'fat', 'sneaky', 'smart', 'abrasive', 'ambiguous', 'deadly', 'determined', 'bawdy', 'cloistered', 'capricious', 'craven', 'dapper', 'debonair', 'divirgent', 'draconian', 'elated', 'erratic', 'fallacious', 'garrulous', 'great', 'greedy', 'incandescent', 'languid', 'macabre', 'twisted', 'green', 'blue', 'black', 'grey', 'pink']
+  const nounList = ['fox', 'cat', 'grandma', 'grandpa', 'dog', 'hound', 'chicken', 'chupacabra', 'lampost', 'rooster', 'wolf', 'vampire', 'bannana', 'peach', 'pear', 'apple', 'duck', 'driver', 'nurse', 'nutcase', 'elephant', 'rat', 'mouse', 'lion', 'kitty', 'kitten', 'cub', 'llama', 'mom', 'dad', 'sister', 'panda', 'sheep', 'tree', 'fern', 'lilly']
 
   const randomArrSelector = (arr) => {
     return Math.floor(Math.random() * arr.length)
   }
   const randomAdj = adjList[randomArrSelector(adjList)]
   const randomNoun = nounList[randomArrSelector(nounList)]
-  const randomName = `${randomAdj} ${randomNoun}`
-  const [name, setName] = useState(randomName)
+  const randomName = () => `${randomAdj} ${randomNoun}`
+  const [name, setName] = useState(isLocalStorageSupported() && checkLocalStorageNameVar() ? window.localStorage.getItem('catClickerUserName') : randomName)
 
+  console.log(window.localStorage)
+  checkLocalStorageNameVar()
   return (
     <Router>
       <Switch>
@@ -32,6 +53,7 @@ function App () {
           <StartMenu
             name={name}
             setName={setName}
+            randomName={randomName}
           />
         </Route>
         <Route path='/game'>
--- src/components/startMenu/StartMenu.js
+++ src/components/startMenu/StartMenu.js
@@ -1,15 +1,41 @@
-import { Link } from "react-router-dom"
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function StartMenu (props) {
+  const [toggleEditName, setToggleEditName] = useState(false)
   return (
-    <>
+    <div
+      style={{ backgroundColor: 'pink' }}
+    >
       <label htmlFor='gamerTag'>Gamer Tag</label>
-      <input id='gamerTag' placeholder={props.name} onChange={(e) => props.setName(e.target.value)} />
+      {toggleEditName === false &&
+        <>
+          <div onClick={() => setToggleEditName(true)}>
+            {props.name}
+          </div>
+          <button
+            onClick={() => props.setName(props.randomName)}
+          >
+            random username
+          </button>
+        </>}
+      {toggleEditName === true &&
+        <>
+          <input
+            id='gamerTag'
+            onBlur={() => setToggleEditName(false)}
+            onInput={(e) => {
+              props.setName(e.target.value)
+              window.localStorage.setItem('catClickerUserName', e.target.value)
+            }}
+          />
+          <button onClick={() => setToggleEditName(false)}>submit</button>
+        </>}
       <Link to='/game' className='paper-btn'>Start Game</Link>
       <Link to='/' className='paper-btn'>Options</Link>
       <h6>{props.name}</h6>
       <label> Select Match Time Limit </label>
-    </>
+    </div>
   )
 }
 export default StartMenu
