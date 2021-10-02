8279b4a5b4cf522907bf03b54727c778242208d8
Gabriel Sokol <gsokol4@gmail.com>
2021-10-02 00:38:18 -0500

    worked on testing app using cypress

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,21 @@
+// sample.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('my first test', () => {
+  it('checks for contains type', () => {
+    cy.visit('example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url()
+      .should('include', '/commands/actions')
+
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
